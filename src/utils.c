b5c1c6643447239ff406683eec8796b0911af0b3
harve592 <harve592@umn.edu>
2023-10-03 02:41:24 -0500

    started

--- src/utils.c
+++ src/utils.c
@@ -1,17 +1,45 @@
 #include "utils.h"
+#include <string.h>
 
-/*
-Use PATH_MAX when creating char arrays for file names
-
-
-You should be using fork(), wait(), and exec() to create the Merkle tree using a process tree
-*/
+// Use PATH_MAX when creating char arrays for file names
 
 // Create N files and distribute the data from the input file evenly among them
 // See section 3.1 of the project writeup for important implementation details
 void partition_file_data(char *input_file, int n, char *blocks_folder)
 {
     // Hint: Use fseek() and ftell() to determine the size of the file
+
+    FILE *fp = fopen(input_file, "r");
+    if (fp == NULL)
+    {
+        printf("ERROR: file not found\n");
+        exit(1);
+    }
+
+    // get size of file
+    fseek(fp, 0L, SEEK_END);
+    long size = ftell(fp);
+
+    // reset fp
+    fseek(fp, 0L, SEEK_SET);
+
+    long blockSize = ceil((float)size / (float)n);
+
+    // create N files
+    for (int x = 0; x < n; x++)
+    {
+        char *argv[] = {"touch", blocks_folder, "hash", n, NULL}; // figure out how to name files
+        if (execvp(*argv, argv) < 0)
+        {
+            printf("ERROR: exec failed\n");
+            exit(1);
+        }
+    }
+
+    // distribute data
+    // use fseek, fread, fwrite
+
+    fclose(fp);
 }
 
 // ##### DO NOT MODIFY THIS FUNCTION #####
