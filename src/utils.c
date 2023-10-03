71d3fc85d0a294f54b5f84d24bd9a1f59df19daf
harve592 <harve592@umn.edu>
2023-10-03 01:49:53 -0500

    init

--- src/utils.c
+++ src/utils.c
@@ -1,31 +1,44 @@
 #include "utils.h"
 
+/*
+Use PATH_MAX when creating char arrays for file names
+
+
+You should be using fork(), wait(), and exec() to create the Merkle tree using a process tree
+*/
+
 // Create N files and distribute the data from the input file evenly among them
 // See section 3.1 of the project writeup for important implementation details
-void partition_file_data(char *input_file, int n, char *blocks_folder) {
+void partition_file_data(char *input_file, int n, char *blocks_folder)
+{
     // Hint: Use fseek() and ftell() to determine the size of the file
 }
 
-
 // ##### DO NOT MODIFY THIS FUNCTION #####
-void setup_output_directory(char *block_folder, char *hash_folder) {
+void setup_output_directory(char *block_folder, char *hash_folder)
+{
     // Remove output directory
     pid_t pid = fork();
-    if (pid == 0) {
+    if (pid == 0)
+    {
         char *argv[] = {"rm", "-rf", "./output/", NULL};
-        if (execvp(*argv, argv) < 0) {
+        if (execvp(*argv, argv) < 0)
+        {
             printf("ERROR: exec failed\n");
             exit(1);
         }
         exit(0);
-    } else {
+    }
+    else
+    {
         wait(NULL);
     }
 
     sleep(1);
 
     // Creating output directory
-    if (mkdir("output", 0777) < 0) {
+    if (mkdir("output", 0777) < 0)
+    {
         printf("ERROR: mkdir output failed\n");
         exit(1);
     }
@@ -33,13 +46,15 @@ void setup_output_directory(char *block_folder, char *hash_folder) {
     sleep(1);
 
     // Creating blocks directory
-    if (mkdir(block_folder, 0777) < 0) {
+    if (mkdir(block_folder, 0777) < 0)
+    {
         printf("ERROR: mkdir output/blocks failed\n");
         exit(1);
     }
 
     // Creating hashes directory
-    if (mkdir(hash_folder, 0777) < 0) {
+    if (mkdir(hash_folder, 0777) < 0)
+    {
         printf("ERROR: mkdir output/hashes failed\n");
         exit(1);
     }
