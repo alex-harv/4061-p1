0a43db57a21d088a74d4bee1490da16d55bbdc63
harve592 <harve592@umn.edu>
2023-10-13 22:04:30 -0500

    updated to marwanes and added debugging

--- src/child_process.c
+++ src/child_process.c
@@ -7,106 +7,149 @@
 
 #define PATH_MAX 1024
 
-
-int main(int argc, char *argv[])
-{
-    if (argc != 5)
-    {
+int main(int argc, char* argv[]) {
+    if (argc != 5) {
         printf("Usage: ./child_process <blocks_folder> <hashes_folder> <N> <child_id>\n");
         return 1;
     }
 
+    // TODO: If the current process is a leaf process, read in the associated block file 
+    // and compute the hash of the block.
+
+    // TODO: If the current process is not a leaf process, spawn two child processes using  
+    // exec() and ./child_process. 
+
+    // TODO: Wait for the two child processes to finish
+
+    // TODO: Retrieve the two hashes from the two child processes from output/hashes/
+    // and compute and output the hash of the concatenation of the two hashes.
+    
+    // Command-line arguments into local variable
     char *blocks_folder = argv[1];
     char *hashes_folder = argv[2];
     int input_n = atoi(argv[3]);
-    char* n;
     int input_child_id = atoi(argv[4]);
-    char* child_id;
+    char block_filename[PATH_MAX];
+    char hash_filename[PATH_MAX];
+    char result_hash[SHA256_BLOCK_SIZE * 2 + 1];
 
 
-    /*
-    ./child_process blocks_folder hashes_folder n child_id
-    where n=leafs and child_id = id of the child
-    */
-
-
-    
-    
-
-    /*
-        left=child (pid==0)
-        right=parent (pid>0)
-        left: child_id = 2*parent_id + 1
-        right: child_id = 2*parent_id + 2
-        right: waits for left to finish then exits
-        both: n = input_n/2
-    */
-    //converting to pass to command line arguments
-    
 
     pid_t pid = fork();
-    if (pid == 0)
-    {
-        //this branch is the left child and exits before the right child process
+    if (pid == 0) {
+        //left tree 
+        if (input_n == 1) {
+            //this is a leaf node
+            snprintf(block_filename, sizeof(block_filename), "%s/%d.txt", blocks_folder, input_child_id);
+            hash_data_block(result_hash, block_filename);
 
-        if (input_n == 1){
-            //this is a leaf node process
-            //TODO: hash the file
-            // how do we hash the file corresponding to 
-            //filename =child_id
-
+            snprintf(hash_filename, sizeof(hash_filename), "%s/%d.txt", hashes_folder, input_child_id);
+            FILE *f = fopen(hash_filename, "w");
+            if (!f) {
+                perror("Error: Opening hash file");
+                exit(1);
+            }
+            fprintf(f, "%s", result_hash);
+            printf("left tree leaf node hash written\n");
+            fclose(f);
             exit(0);
         }
+        //this is not a leaf node
         
-        //this is a non-leaf node process
-        //TODO: hash the two children
-        
-        
-        
-        //converting to pass to command line arguments
-        sprintf(child_id, "%d", input_child_id*2+1);
-        sprintf(n, "%d", input_n/2); 
+        int left_child_id = 2 * input_child_id + 1;
+        char n[10];
+        char child_id[10];
+        snprintf(n, sizeof(n), "%d", input_n / 2);
+        snprintf(child_id, sizeof(child_id), "%d", left_child_id);
+        printf("left tree recursion n= %s child_id= %s\n",n,child_id);
 
-        // ./child_process <blocks_folder> <hashes_folder> <N> <child_id>
-        char *argv2[] = {"./child_process", blocks_folder, hashes_folder, n, child_id, NULL};
-        if (execvp(*argv2, argv2) < 0)
-        {
-            // i hope we can use execvp instead of regular exec
-            // guide says use exec()
+        char *argv[] = {"./child_process", blocks_folder, hashes_folder, n, child_id, NULL};
+        if (execvp(*argv, argv) < 0) {
             printf("ERROR: exec failed at child_id:%s, and N:%s \n", child_id, n);
             exit(1);
         }
         exit(0);
-    }
+    } 
 
 
-    else
-    {
-        //this branch is the right child and waits for left to finish then exits
-        if (input_n == 1){
-            //this is a leaf node process
-            //TODO: hash the file
 
+
+    else {
+        //right tree
+        if (input_n == 1) {
+            //this is a leaf node
+            snprintf(block_filename, sizeof(block_filename), "%s/%d.txt", blocks_folder, input_child_id);
+            hash_data_block(result_hash, block_filename);
+
+            snprintf(hash_filename, sizeof(hash_filename), "%s/%d.txt", hashes_folder, input_child_id);
+            FILE *f = fopen(hash_filename, "w");
+            if (!f) {
+                perror("Error: Opening hash file");
+                exit(1);
+            }
+            
+            fprintf(f, "%s", result_hash);
+            printf("right tree leaf node hash written\n");
+
+            fclose(f);
+            
             exit(0);
+           
         }
+        //this is not a leaf node
         
-        //this is a non-leaf node process
-        //TODO: hash the two children
-
         
-        sprintf(child_id, "%d", input_child_id*2+2);
-        sprintf(n, "%d", input_n/2); 
 
-        // ./child_process <blocks_folder> <hashes_folder> <N> <child_id>
-        char *argv2[] = {"./child_process", blocks_folder, hashes_folder, n, child_id, NULL};
-        if (execvp(*argv2, argv2) < 0)
-        {
+        int right_child_id = 2 * input_child_id + 2;
+        char n[10];
+        char child_id[10];
+        snprintf(n, sizeof(n), "%d", input_n / 2);
+        snprintf(child_id, sizeof(child_id), "%d", right_child_id);
+
+        printf("right tree recursion n= %s child_id= %s\n",n,child_id);
+        char *argv[] = {"./child_process", blocks_folder, hashes_folder, n, child_id, NULL};
+        if (execvp(*argv, argv) < 0) {
             printf("ERROR: exec failed at child_id:%s, and N:%s \n", child_id, n);
             exit(1);
         }
 
-        // wait for left to finish
+        char left_hash[SHA256_BLOCK_SIZE * 2 + 1];
+        char right_hash[SHA256_BLOCK_SIZE * 2 + 1];
+
+        snprintf(hash_filename, sizeof(hash_filename), "%s/%d.txt", hashes_folder, 2 * input_child_id + 1);
+        FILE *left_f = fopen(hash_filename, "r");
+        if (!left_f) {
+            perror("Error: Opening left hash");
+            exit(1);
+        }
+        fscanf(left_f, "%s", left_hash);
+        fclose(left_f);
+
+        snprintf(hash_filename, sizeof(hash_filename), "%s/%d.txt", hashes_folder, 2 * input_child_id + 2);
+        FILE *right_f = fopen(hash_filename, "r");
+        if (!right_f) {
+            perror("Error: Opening right hash");
+            exit(1);
+        }
+        fscanf(right_f, "%s", right_hash);
+        fclose(right_f);
+
+
+        compute_dual_hash(result_hash, left_hash, right_hash);
+        printf("computed dual hash in right tree\n");
+
+        snprintf(hash_filename, sizeof(hash_filename), "%s/%d.txt", hashes_folder, input_child_id);
+        FILE *f = fopen(hash_filename, "w");
+        if (!f) {
+            perror("Error: Opening hash file");
+            exit(1);
+        }
+        fprintf(f, "%s", result_hash);
+        fclose(f);
+
+
         wait(NULL);
         exit(0);
+        printf("right tree parent exited\n");
     }
 }
--- src/merkle.c
+++ src/merkle.c
@@ -6,34 +6,40 @@ char *blocks_folder = "output/blocks";
 char *hashes_folder = "output/hashes";
 char *visualization_file = "output/visualization.txt";
 
-int main(int argc, char *argv[])
-{
-    if (argc != 3)
-    {
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
         // N is the number of data blocks to split <file> into (should be a power of 2)
         // N will also be the number of leaf nodes in the merkle tree
         printf("Usage: ./merkle <file> <N>\n");
         return 1;
     }
 
-    // TODO: validate command line arguments
-    char *input_file = argv[1];
+    // TODO: Read in the command line arguments and validate them
+    char *input_file = argv [1];
     int n = atoi(argv[2]);
+
 
     // ##### DO NOT REMOVE #####
     setup_output_directory(blocks_folder, hashes_folder);
+
+    // TODO: Implement this function in utils.c
     partition_file_data(input_file, n, blocks_folder);
 
-    // Start the recursive merkle tree computation by spawning first child process (root)
-    // ./child_process <blocks_folder> <hashes_folder> <N> <child_id>
-    char *argv2[] = {"./child_process", blocks_folder, hashes_folder, n, 0, NULL};
-    execvp(*argv2, argv2);
 
-// ##### DO NOT REMOVE #####
-#ifndef TEST_INTERMEDIATE
-    // Visually display the merkle tree using the output in the hashes_folder
-    print_merkle_tree(visualization_file, hashes_folder, n);
-#endif
+    // TODO: Start the recursive merkle tree computation by spawning first child process (root)
+    char string_n[10];
+    char *argv2[] = {"./child_process", blocks_folder, hashes_folder, string_n, "0", NULL};
+    if(execvp(argv2[0], argv2) == -1) {
+    	perror("Error: starting child process");
+   	 return -1;
+    }
+
+    // ##### DO NOT REMOVE #####
+    #ifndef TEST_INTERMEDIATE
+        // Visually display the merkle tree using the output in the hashes_folder
+        print_merkle_tree(visualization_file, hashes_folder, n);
+    #endif
 
     return 0;
 }
